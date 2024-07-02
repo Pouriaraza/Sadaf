f0385396eb8468cca1371e845028a30e02a637e5
PouriaRAZA <pooria.pas1@gmail.com>
2024-07-02 16:42:10 +0330

    hamberger menu -v6

--- app/(primary)/work/page.js
+++ app/(primary)/work/page.js
@@ -1,13 +1,11 @@
 import Navbar from "@/app/components/Navbar";
 import "./style.scss";
-import Elastic from "@/app/components/Elastic";
 
 const Home = () => {
 
     return (
         <section className="work">
             <Navbar />
-            <Elastic />
         </section>
     );
 }
